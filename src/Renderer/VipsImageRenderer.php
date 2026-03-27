14c14cc927777b088088c073ae5e08fccee7b952
Daniel Dahme <danieldahme@googlemail.com>
2026-03-27 21:25:20 +0000

    header added

--- src/Renderer/VipsImageRenderer.php
+++ src/Renderer/VipsImageRenderer.php
@@ -1,5 +1,19 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
+
+/***********************************************************************
+ * This file is part of VipsRenderer for BASE3 Framework.
+ *
+ * VipsRenderer extends the BASE3 framework with a libvips-based image
+ * rendering backend for file-based image transformations and exports.
+ * It provides fast rendering for publish-ready image derivatives.
+ *
+ * Developed by Daniel Dahme
+ * Licensed under GPL-3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * https://base3.de/v/vipsrenderer
+ * https://github.com/ddbase3/VipsRenderer
+ **********************************************************************/
 
 namespace VipsRenderer\Renderer;
 
